124fdb8df2321d0cc4a19c92438713cc2599b608
Laurin <laurin.ebnoether@gmail.com>
2024-12-14 17:51:28 +0100

    Add project files.

---
+++ csv-to-json/CsvConverter.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * ----------------------------------------------------------------------------
+ * File: CsvConverter.cs
+ * Author: Laurin Ebnöther
+ * Created: 14.12.2024
+ * Description: Konvertiert CSV in JSON datei
+ *
+ * Version: 1.0
+ * ----------------------------------------------------------------------------
+ */
+
+using CsvHelper.Configuration;
+using CsvHelper;
+using System.Globalization;
+using System.Text.Json;
+
+namespace csv_to_json
+{
+    public class CsvConverter
+    {
+
+        // Convertiert CSV to Json mithilfe eines CsvReaders und eine JsonSerializer
+        public static string ConvertCsvToJson(Stream csvStream, string delimiter)
+        {
+            try
+            {
+                CsvConfiguration Config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    Delimiter = delimiter,
+                    HasHeaderRecord = true,
+                };
+
+                using StreamReader StreamReader = new StreamReader(csvStream);
+                using CsvReader CsvReader = new CsvReader(StreamReader, Config);
+                List<dynamic> CsvData = CsvReader.GetRecords<dynamic>().ToList();
+
+                return JsonSerializer.Serialize(CsvData, new JsonSerializerOptions
+                {
+                    WriteIndented = true
+                });
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Fehler beim konvertieren: {e.Message}", e);
+            }
+        }
+
+    }
+}
---
+++ csv-to-json/Function.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * ----------------------------------------------------------------------------
+ * File: Function.cs
+ * Author: Laurin Ebnöther
+ * Created: 14.12.2024
+ * Description: Schnittstelle zwischen Code und AWS Cloud
+ *
+ * Version: 1.0.1
+ * ----------------------------------------------------------------------------
+ */
+
+using Amazon.Lambda.Core;
+using Amazon.Lambda.S3Events;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.Lambda.Serialization.SystemTextJson; 
+
+[assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
+
+namespace csv_to_json
+{
+    public class Function
+    {
+        private readonly IAmazonS3 S3Client;
+        public string DestinationBucketName { get; set; }
+        public string CsvDelimiter { get; set; }
+
+        // Konstruktor wird von AWS Lambda benötigt!
+        // Konstruktor erstellt S3Client instanz und eine Config instanz.
+        public Function()
+        {
+            S3Client = new AmazonS3Client();
+            DestinationBucketName = Environment.GetEnvironmentVariable("DESTINATION_BUCKET") 
+                ?? throw new InvalidOperationException("Umgebungsvariable fehlt: DESTINATION_BUCKET");
+            CsvDelimiter = Environment.GetEnvironmentVariable("CSV_DELIMITER") ?? ",";
+
+        }
+
+        public async Task FunctionHandler(S3Event evnt, ILambdaContext context)
+        {
+            foreach (var record in evnt.Records)
+            {
+                try
+                {
+                    string bucketName = record.S3.Bucket.Name;
+                    string fileName = record.S3.Object.Key;
+
+                    //csv Datei von Bucket holen
+                    GetObjectRequest csvFile = new GetObjectRequest
+                    {
+                        BucketName = bucketName,
+                        Key = fileName
+                    };
+
+                    var response = await S3Client.GetObjectAsync(csvFile);
+                    string jsonContent = CsvConverter.ConvertCsvToJson(response.ResponseStream, CsvDelimiter);
+                    string outputKey = Path.ChangeExtension(fileName, ".json");
+
+                    // Json File in Output Bucket laden
+                    var putRequest = new PutObjectRequest
+                    {
+                        BucketName = DestinationBucketName,
+                        Key = outputKey,
+                        ContentType = "application/json",
+                        ContentBody = jsonContent
+                    };
+                    await S3Client.PutObjectAsync(putRequest);
+                }
+                catch (Exception e)
+                {
+                    throw new FileNotFoundException($"File wurde nicht gefunden! {e.Message}");
+                }
+            }
+        }
+
+    }
+}
